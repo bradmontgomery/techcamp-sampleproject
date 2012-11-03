e2366e70c2837b26459b1518852755ad3a8f4ce2
Brad Montgomery <brad@bradmontgomery.net>
2012-11-02 19:59:00 -0500

    added an index for Posts

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,22 @@ from models import Post
 
 
 def display_post(request, post_slug):
-    
+
     post = Post.objects.get(slug=post_slug)
-    
+
     template_data = {'post': post}
     template = "blog/post.html"
-    
+
+    return render_to_response(
+        template,
+        template_data,
+        context_instance=RequestContext(request)
+    )
+
+
+def list_posts(request):
+    template_data = {'posts': Post.objects.all()}
+    template = "blog/index.html"
     return render_to_response(
         template,
         template_data,
--- sampleproject/urls.py
+++ sampleproject/urls.py
@@ -7,5 +7,8 @@ urlpatterns = patterns('',
     url(r'^blog/(?P<post_slug>.+)/$',
         'blog.views.display_post',
         name='display_post'),
+    url(r'^$',
+        'blog.views.list_posts',
+        name='list_posts'),
     url(r'^admin/', include(admin.site.urls)),
 )
